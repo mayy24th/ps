506802385cb27277887316c95554eb06db573007
mayy24th <86235986+mayy24th@users.noreply.github.com>
2024-09-27 15:03:42 +0900

    boj 2562

---
+++ 백준/2562.java
@@ -0,0 +1,20 @@
+package org.elice;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        List<Integer> list = new ArrayList<>();
+        for(int i=0; i<9; i++) list.add(Integer.parseInt(br.readLine()));
+        int max = Collections.max(list);
+        System.out.println(max);
+        System.out.println(list.indexOf(max) + 1);
+    }
+}
